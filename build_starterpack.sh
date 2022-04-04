ed307b89a7bef9badbf811c0f64d51c4463ef13b
Entelect <fabio.loreggian@entelect.co.za>
2022-04-04 12:28:48 +0200

    Updated starter bot build

--- build_starterpack.sh
+++ build_starterpack.sh
@@ -5,9 +5,7 @@ mkdir starter-pack
 cp -r ./engine-publish ./starter-pack/
 cp -r ./runner-publish ./starter-pack/
 cp -r ./logger-publish ./starter-pack/
-cp -r ./reference-bot-publish ./starter-pack/
 cp -r ./starter-bots ./starter-pack/
-cp -r ./visualiser ./starter-pack/
 
 cp ./starter-bots/README.md ./starter-pack/
 cp ./building-a-bot.md ./starter-pack/
