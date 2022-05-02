531f1e5ace4cbeb7b9f16c847af9844585d908a9
Frik van der Merwe <gatkramp@gmail.com>
2022-05-02 14:28:07 +0200

    update requirements.txt
    update requirements.txt to include signalcore_async. Also, comment cleanup in StarterBot.py

--- starter-bots/PythonBot/StarterBot.py
+++ starter-bots/PythonBot/StarterBot.py
@@ -44,87 +44,6 @@ def set_hub_connection(connected) -> None:
     global hub_connected
     hub_connected = connected
 
-"""
-private static async Task Main(string[] args)
-    {
-
-        
-
-
-
-
-
-        var botService = new BotService();
-
-        await connection.StartAsync()
-            .ContinueWith(
-                task =>
-                {
-                    Console.WriteLine("Connected to Runner");
-                    /* Clients should disconnect from the server when the server sends the request to do so. */
-                    connection.On<Guid>(
-                        "Disconnect",
-                        (id) =>
-                        {
-                            Console.WriteLine("Disconnected:");
-
-                            connection.StopAsync();
-                            connection.DisposeAsync();
-                        });
-                    connection.On<Guid>(
-                        "Registered",
-                        (id) =>
-                        {
-                            Console.WriteLine("Registered Bot with the runner");
-                            botService.SetBot(
-                                new BotDto()
-                                {
-                                    Id = id
-                                });
-                        });
-
-                    /* Get the current WorldState along with the last known state of the current client. */
-                    connection.On<GameStateDto>(
-                        "ReceiveBotState",
-                        (gameStateDto) =>
-                        {
-                            Console.WriteLine("GameStateDTO hit");
-                            var gameState = new GameState { World = null, Bots = new List<BotDto>() };
-                            gameState.World = gameStateDto.World;
-                            gameState.Bots = gameStateDto.Bots;
-
-                            botService.SetGameState(gameState);
-                        });
-
-                    var token = Environment.GetEnvironmentVariable("REGISTRATION_TOKEN");
-                    token = !string.IsNullOrWhiteSpace(token) ? token : Guid.NewGuid().ToString();
-
-                    Thread.Sleep(1000);
-                    Console.WriteLine("Registering with the runner...");
-                    connection.SendAsync("Register", token, "NetNickName");
-
-                    while (connection.State == HubConnectionState.Connected)
-                    {
-                        Thread.Sleep(30);
-                        Console.WriteLine($"ConState: {connection.State}");
-                        Console.WriteLine($"Bot: {botService.GetBot()?.Id.ToString()}");
-
-                        var bot = botService.GetBot();
-                        if (bot == null)
-                        {
-                            continue;
-                        }
-
-                        if (botService.GetGameState().World != null)
-                        {
-                            botService.ComputeNextPlayerAction(botService.GetPlayerCommand());
-                            connection.InvokeAsync("SendPlayerCommand", botService.GetPlayerCommand());
-                        }
-                    }
-                });
-    }
-
-"""
 async def run_bot() -> None:
     #get registrationToken
     token = os.getenv("REGISTRATION_TOKEN")
@@ -176,36 +95,7 @@ async def run_bot() -> None:
         
         await hub_connection.stop()
     
-    
-        
-    #I am here
 
-    # hub_connection.on_open(lambda: (print("Connection opened and handshake received, ready to send messages"),
-    #                                 set_hub_connection(True)))
-    # hub_connection.on_error(lambda data: print(f"An exception was thrown closed: {data.error}"))
-    # hub_connection.on_close(lambda: (print("Connection closed"),
-    #                                  set_hub_connection(False)))
-
-    # hub_connection.on("Registered", on_register)
-    # hub_connection.on("ReceiveBotState", get_next_player_action)
-    # hub_connection.on("Disconnect", lambda data: (print("Disconnect Called"),(set_hub_connection(False))))
-    # hub_connection.on("ReceiveGameComplete", lambda data: (print("Game complete"), (on_disconnect)))
-
-    
-    # time.sleep(1)
-
-
-
-    # print("Registering with the runner...")
-    
-    # registration_args = [str(token), bot_nickname]
-    # hub_connection.send("Register", registration_args)
-
-    # time.sleep(5)
-    # while hub_connected:
-    #     continue
-       
-    # hub_connection.stop()
 
 def get_next_player_action(args) -> None: 
     #Get the current WorldState along with the last known state of the current client. 
